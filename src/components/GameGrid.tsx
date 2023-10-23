deed024faa34889340093b878c7e78ae7d3b1907
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-23 14:03:41 -0400

    Create a custom hook to fetch the game

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,40 +1,14 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { Text } from "@chakra-ui/react"
+import { useGames } from "../hooks/useGames.ts";
 
 export const GameGrid = () => {
-// set up states for games, loading, and error
-  const [games, setGames] = useState<Game[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+    const {games, loading, error} = useGames()
 
-  useEffect(() => {
-    //Fetch games from the API
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .then(() => {
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setLoading(false);
-      });
-  });
 
   return (
     <>
-      {loading && <text>Loading...</text>}
-      {error && <text>{error}</text>}
+      {loading && <Text>Loading...</Text>}
+      {error && <Text>{error}</Text>}
       <ul>
         {games.map((game) => (
           <li key={game.id}>{game.name}</li>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+  }
+  
+  interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+  }
+
+export const useGames = () => {
+    // set up states for games, loading, and error
+  const [games, setGames] = useState<Game[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    //Fetch games from the API
+    apiClient
+      .get<FetchGamesResponse>("/games", {signal: controller.signal})
+      .then((res) => setGames(res.data.results))
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+      return () => {
+        controller.abort();
+      }
+  }, []);
+
+  return { games, loading, error };
+};
