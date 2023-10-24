8267606390836287559f25f2ce8fbdc2e8ca6a5e
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-24 19:17:11 -0400

    reate a generic data fetching hook

---
+++ src/hooks/useData.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface FetchResponse<T> {
+    count: number;
+    results: T[];
+}
+
+const useData = <T>(endpoint: string) => {
+     // set up states for games, loading, and error
+  const [data, setData] = useState<T[]>([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    //Fetch games from the API
+    setLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setData(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return { data, isLoading, error}
+}
+
+export default useData;
