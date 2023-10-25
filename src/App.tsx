ce31f0c2f3dd2b3c3521f0d5a3b1a6b716fea206
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-24 20:19:46 -0400

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 
-
 function App() {
   return (
     <Grid
@@ -11,16 +10,21 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`, // 1024px
       }}
+      templateColumns={{ 
+        base: '1fr',        
+        lg: '200px 1fr', // 1024px
+
+       }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       {/* Show function will display specific elements depending on the user's screen size */}
       <Show above="lg">
-      <GridItem area="aside">
-        <GenreList />
-      </GridItem>
-      </Show> 
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
+      </Show>
       <GridItem area="main">
         <GameGrid />
       </GridItem>
